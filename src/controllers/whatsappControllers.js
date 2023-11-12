48af8bf8f89a8f7a2ca512be37c8a8eab1bd2d69
Manoel Brasil <manoelbo@hotmail.com>
2023-11-11 23:46:44 -0300

    update

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -25,9 +25,10 @@ const ReceivedMessage = (req, res) => {
         var changes = (entry["changes"])[0];
         var value = (changes["value"]);
         var messageObject = value["messages"];
+        var messages = messageObject[0];
         var number = messages["from"];
 
-        console.log('messageObject:',messageObject);
+        console.log('messages:',messages);
        
         var text = "Olá,\n\nSeja bem-vindo ao *Anti Audio Audio Club*. Somos um serviço e uma comunidade ```PREMIUM``` para pessoas que odeiam receber áudios no WhatsApp.\n\n*Como essa budega funciona:*\nSempre que você encaminhar ou enviar um áudio para este contato, nós o transcreveremos para você. Simples assim. \n\nNosso serviço está na fase beta do beta. Não confie na transcrição e não nos xingue.\n\nEstamos trabalhando duro para um mundo com menos áudios do WhatsApp.\n\n#vitórianaguerra"
         
