130dd1ce7801ee8dad2cf806f505e09b8ec343d2
Manoel Brasil <manoelbo@hotmail.com>
2023-11-15 17:06:49 -0300

    Add Firebase Admin SDK initialization to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const apiRoute = require("./routes/routes");
+const admin = require('firebase-admin');
+const serviceAccount = require('../serviceAccountKey.json');
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
