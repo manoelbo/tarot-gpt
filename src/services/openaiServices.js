bcdaf6ac3974419a5a2f4b269627581c35f0035e
Manoel Brasil <manoelbo@hotmail.com>
2023-11-17 12:25:57 -0300

    Fix OpenAI thread execution and handle error in WhatsApp message sending.

--- src/services/openaiServices.js
+++ src/services/openaiServices.js
@@ -8,39 +8,57 @@ const openai = new openAI({
 });
 
 async function createThreadOpenAI(question, cards) {
-  console.log("🚀 ~ file: openaiServices.js:15 ~ createThreadOpenAI ~ createThreadOpenAI:", createThreadOpenAI)
-  // create let content with text pergunta: {question} + break line + carta 1: {card[0]} + break line + carta 2: {card[1]} + break line + carta 3: {card[3]} 
-  let content = `pergunta: ${question}\n`;
-  cards.forEach((card, index) => {
-    content += `carta ${index + 1}: ${card.name}\n`;
-  });
-  console.log("🚀 ~ file: openaiServices.js:19 ~ createThreadOpenAI ~ content:", content)
+  try {
+    console.log("🚀 ~ file: openaiServices.js:15 ~ createThreadOpenAI ~ createThreadOpenAI:", createThreadOpenAI)
+    let content = `${question}\n`;
+    cards.forEach((card, index) => {
+      
+      content += `carta ${index + 1}: ${card.name}\n`;
+    });
 
-  const userThread = await openai.beta.threads.create({
-    messages: [
-      {
-        role: "user",
-        content: content,
-      },
-    ],
-  });
-  // get Thread Object ID if userThread is not null or undefined else return error 
-  return runThreadWithAssistent(userThread);
+    console.log("🚀 ~ file: openaiServices.js:15 ~ createThreadOpenAI ~ content:", content);
+    const userThread = await openai.beta.threads.create({
+      messages: [
+        {
+          role: "user",
+          content: content,
+        },
+      ],
+    });
+
+    return runThreadWithAssistent(userThread);
+  } catch (error) {
+    console.error(error);
+    throw new Error('Erro ao criar thread com a OpenAI');
+  }
 }
 
 async function runThreadWithAssistent(userThread) {
-  const runThread = await openai.beta.threads.runs.create(
-    userThread.id,
-    { assistant_id: "asst_f1mkXd0SJfwlW3oJKDjE0P4H" }
-  );
-  return waitRunToComplete(userThread, runThread);
+  console.log("🚀 ~ file: openaiServices.js:35 ~ runThreadWithAssistent ~ runThreadWithAssistent:", runThreadWithAssistent)
+  try {
+    
+    const runThread = await openai.beta.threads.runs.create(
+      userThread.id,
+      { assistant_id: "asst_f1mkXd0SJfwlW3oJKDjE0P4H" }
+    );
+    return waitRunToComplete(userThread, runThread);
+  } catch (error) {
+    console.error(error);
+    throw new Error('Erro ao executar thread com assistente');
+  }
 }
 
-async function waitRunToComplete(userThread, runThread){
+async function waitRunToComplete(userThread, runThread) {
+  console.log("🚀 ~ file: openaiServices.js:50 ~ waitRunToComplete ~ waitRunToComplete:", waitRunToComplete)
   let run = runThread;
   while (run.status !== "completed") {
     await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(run.status);
     run = await openai.beta.threads.runs.retrieve(userThread.id , run.id);
+    if (run.status === "failed") {
+      console.log(run.last_error.message);
+      throw new Error('A execução de run falhou');
+    }
   }
   if (run.status === "completed") {
     return getMessageCreatedByRunOnThread(userThread, run);
@@ -49,15 +67,23 @@ async function waitRunToComplete(userThread, runThread){
 
 async function getMessageCreatedByRunOnThread(userThread, completedRun){
   
-  const messages = await openai.beta.threads.messages.list(userThread.id);
-  const lastMessageForRun = messages.data
-        .filter(
-          (message) => message.run_id === completedRun.id && message.role === "assistant"
-        )
-        .pop();
-      if (lastMessageForRun) {
-        return lastMessageForRun.content[0].text.value;
-      }
+  try {
+    console.log("🚀 ~ file: openaiServices.js:67 ~ getMessageCreatedByRunOnThread ~ getMessageCreatedByRunOnThread:", getMessageCreatedByRunOnThread)
+    const messages = await openai.beta.threads.messages.list(userThread.id);
+    const lastMessageForRun = messages.data
+          .filter(
+            (message) => message.run_id === completedRun.id && message.role === "assistant"
+          )
+          .pop();
+    if (lastMessageForRun) {
+      return lastMessageForRun.content[0].text.value;
+    } else {
+      throw new Error('Nenhuma mensagem encontrada para a execução concluída');
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error('Erro ao obter a mensagem criada pela execução');
+  }
 }
 
 
